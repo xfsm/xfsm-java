7c5a29b11cd37a9a73fc02dd51f37f590a9d2544
sng2c <sng2nara@gmail.com>
2015-07-28 12:05:17 +0900

    added RuleSet

--- src/main/java/com/mabook/xfsm/XFSM.java
+++ src/main/java/com/mabook/xfsm/XFSM.java
@@ -1,71 +1,43 @@
 package com.mabook.xfsm;
 
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
-/**
- * Created by sng2c on 15. 7. 27..
- */
 public class XFSM {
-
+	public enum When {ENTER, EXIT, TRANSITION};
 	public interface ActionListener{
-		void onAction(XFSM context, String action);
+		void onAction(XFSM context, When when, String action);
 	}
 
-	public static class State{
-		public String name;
-		public String onEnterAction;
-		public String onExitAction;
-
-		public State(String name, String onEnterAction, String onExitAction) {
-			this.name = name;
-			this.onEnterAction = onEnterAction;
-			this.onExitAction = onExitAction;
-		}
-	}
-
-	public static class Transition{
-		public String event;
-		public State fromState;
-		public State toState;
-		public String onTransitAction;
-
-		public Transition(String event, State fromState, State toState, String onTransitAction) {
-			this.event = event;
-			this.fromState = fromState;
-			this.toState = toState;
-			this.onTransitAction = onTransitAction;
-		}
-	}
-
-
-
-	State currentState;
+	RuleSet.State currentState;
 	ActionListener actionListener;
 
-	final State initialState;
-	final HashMap<State, HashMap<String, Transition>> stateMap;
+	final RuleSet ruleSet;
 
-	public XFSM(State initialState, List<Transition> stateList, ActionListener actionListener){
-		this.initialState = initialState;
+	public XFSM(RuleSet ruleSet){
+		this(ruleSet, null);
+	}
 
-		stateMap = new HashMap<>();
-		for ( Transition transition : stateList ){
-			if( !stateMap.containsKey(transition.fromState) ){
-				stateMap.put(transition.fromState, new HashMap<String, Transition>());
-			}
-			stateMap.get(transition.fromState).put(transition.event, transition);
-		}
+	public XFSM(RuleSet ruleSet, ActionListener actionListener){
+		this.ruleSet = ruleSet;
+		this.actionListener = actionListener;
+	}
 
+	public ActionListener getActionListener() {
+		return actionListener;
+	}
+
+	public void setActionListener(ActionListener actionListener) {
 		this.actionListener = actionListener;
 	}
 
 	public String init(){
-		this.currentState = initialState;
+		this.currentState = ruleSet.getInitialState();
 
 		if (currentState.onEnterAction != null) {
 			if (actionListener != null) {
-				actionListener.onAction(this, currentState.onEnterAction);
+				actionListener.onAction(this, When.ENTER, currentState.onEnterAction);
 			}
 		}
 
@@ -74,26 +46,23 @@ public class XFSM {
 
 	public void emit(String event){
 		if( currentState != null ) {
-			HashMap<String, Transition> eventMap = stateMap.get(currentState);
-			if( eventMap != null ) {
-				Transition transition = eventMap.get(event);
-				if (transition != null) {
-					if (actionListener != null) {
-						if (currentState.onExitAction != null) {
-							actionListener.onAction(this, currentState.onExitAction);
-						}
-
-						if (transition.onTransitAction != null) {
-							actionListener.onAction(this, transition.onTransitAction);
-						}
+			RuleSet.Transition transition = ruleSet.getTransition(currentState, event);
+			if (transition != null) {
+				if (actionListener != null) {
+					if (currentState.onExitAction != null) {
+						actionListener.onAction(this, When.EXIT, currentState.onExitAction);
 					}
 
-					currentState = transition.toState;
+					if (transition.onTransitAction != null) {
+						actionListener.onAction(this, When.TRANSITION, transition.onTransitAction);
+					}
+				}
 
-					if (actionListener != null) {
-						if (currentState.onEnterAction != null) {
-							actionListener.onAction(this, currentState.onEnterAction);
-						}
+				currentState = transition.toState;
+
+				if (actionListener != null) {
+					if (currentState.onEnterAction != null) {
+						actionListener.onAction(this, When.ENTER, currentState.onEnterAction);
 					}
 				}
 			}
@@ -103,21 +72,18 @@ public class XFSM {
 	public List<String> run(String event){
 		ArrayList<String> actions = new ArrayList<>();
 		if( currentState != null ) {
-			HashMap<String, Transition> eventMap = stateMap.get(currentState);
-			if( eventMap != null ) {
-				Transition transition = eventMap.get(event);
-				if (transition != null) {
-					if (currentState.onExitAction != null)
-						actions.add(currentState.onExitAction);
+			RuleSet.Transition transition = ruleSet.getTransition(currentState, event);
+			if (transition != null) {
+				if (currentState.onExitAction != null)
+					actions.add(currentState.onExitAction);
 
-					if (transition.onTransitAction != null)
-						actions.add(transition.onTransitAction);
+				if (transition.onTransitAction != null)
+					actions.add(transition.onTransitAction);
 
-					currentState = transition.toState;
+				currentState = transition.toState;
 
-					if (currentState.onEnterAction != null)
-						actions.add(currentState.onEnterAction);
-				}
+				if (currentState.onEnterAction != null)
+					actions.add(currentState.onEnterAction);
 			}
 		}
 		return actions;
--- src/test/java/com/mabook/xfsm/XFSMTest.java
+++ src/test/java/com/mabook/xfsm/XFSMTest.java
@@ -5,12 +5,10 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import javax.swing.*;
-
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertArrayEquals;
 
 /**
  * Created by sng2c on 15. 7. 27..
@@ -28,15 +26,18 @@ public class XFSMTest extends TestCase {
 	}
 
 	@Test
-	public void testNew(){
-		XFSM.State init = new XFSM.State("init", "at home", "at street");
-		XFSM.State hello = new XFSM.State("hello", "say hello", "say bye");
+	public void testRun(){
 
-		ArrayList<XFSM.Transition> transitions = new ArrayList<>();
-		transitions.add(new XFSM.Transition("go out", init, hello, "take a taxi"));
-		transitions.add(new XFSM.Transition("go home", hello, init, "take a bus"));
+		RuleSet ruleSet = new RuleSet();
+		ruleSet
+				.registerState("init", "at home", "at street")
+				.registerState("hello", "say hello", "say bye")
+				.setInitialStateName("init")
+				.registerEvent("go out", "init", "hello", "take a taxi")
+				.registerEvent("go home", "hello", "init", "take a bus");
 
-		XFSM fsm = new XFSM(init, transitions, null);
+
+		XFSM fsm = new XFSM(ruleSet);
 		String initAction = fsm.init();
 		assertEquals("initial action", initAction, "at home");
 
@@ -46,6 +47,36 @@ public class XFSMTest extends TestCase {
 		assertArrayEquals("go out actions", actions.toArray(), new String[]{"at street", "take a taxi", "say hello"});
 
 		actions = fsm.run("go home");
+		assertArrayEquals("go home actions", actions.toArray(), new String[]{"say bye", "take a bus", "at home"});
+	}
+
+	public void testEmit(){
+		RuleSet ruleSet = new RuleSet();
+		ruleSet
+				.registerState("init", "at home", "at street")
+				.registerState("hello", "say hello", "say bye")
+				.setInitialStateName("init")
+				.registerEvent("go out", "init", "hello", "take a taxi")
+				.registerEvent("go home", "hello", "init", "take a bus");
+
+
+		final List<String> actions = new ArrayList<>();
+		XFSM fsm = new XFSM(ruleSet);
+		fsm.setActionListener(new XFSM.ActionListener() {
+			@Override
+			public void onAction(XFSM context, XFSM.When when, String action) {
+				actions.add(action);
+			}
+		});
+		fsm.init();
+		assertEquals("initial action", actions.get(0), "at home");
+		actions.clear();
+
+		fsm.emit("go out");
+		assertArrayEquals("go out actions", actions.toArray(), new String[]{"at street", "take a taxi", "say hello"});
+		actions.clear();
+
+		fsm.emit("go home");
 		assertArrayEquals("go home actions", actions.toArray(), new String[]{"say bye", "take a bus", "at home"});
 	}
 }
