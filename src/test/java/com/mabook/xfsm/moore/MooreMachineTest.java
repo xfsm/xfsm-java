5746946d095d0d66ba535d663c995844df717768
hanson.pl <hanson.pl@kakaobank.com>
2019-09-10 18:59:10 +0900

    Split exception test

--- src/test/java/com/mabook/xfsm/moore/MooreMachineTest.java
+++ src/test/java/com/mabook/xfsm/moore/MooreMachineTest.java
@@ -9,13 +9,30 @@ import static com.mabook.xfsm.moore.MooreMachine.*;
 import static org.junit.Assert.*;
 
 public class MooreMachineTest {
-    @Test(expected = NoRoutesException.class)
+    @Test()
     public void testTrasition() throws NoRoutesException {
         Map<String, Map<String, String>> map = MAP("S0", MAP("E0", "S1"));
         MooreMachine mm = new MooreMachine(map);
         String newState = mm.transit("S0", "E0");
         assertEquals("S1", newState);
 
-        newState = mm.transit("S1", "E0");
     }
+
+    @Test(expected = NoRoutesException.class)
+    public void testTrasitionException1() throws NoRoutesException {
+        Map<String, Map<String, String>> map = MAP("S0", MAP("E0", "S1"));
+        MooreMachine mm = new MooreMachine(map);
+
+        // throws NoRoutesException
+        mm.transit("S1", "E0");
+    }
+    @Test(expected = NoRoutesException.class)
+    public void testTrasitionException2() throws NoRoutesException {
+        Map<String, Map<String, String>> map = MAP("S0", MAP("E0", "S1"));
+        MooreMachine mm = new MooreMachine(map);
+
+        // throws NoRoutesException
+        mm.transit("S0", "E1");
+    }
+
 }
